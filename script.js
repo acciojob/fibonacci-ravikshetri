c044e129b789077dab800140699205e3eb51d085
ravikshetri <112544266+ravikshetri@users.noreply.github.com>
2023-04-17 01:10:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,14 +1,13 @@
 function fibonacci(num) {
 // your code here\
-	let fib = [];
      fib[0] = 0;
      fib[1] = 1;
   
-  for (let i = 2; i <= n; i++) {
-    fib[i] = fib[i - 1] + fib[i - 2];
+    for (let i = 2; i <= n; i++) {
+    num[i] = num[i - 1] + num[i - 2];
   }
 
-  return fib;
+  return num;
 	
 }
 
